af9c21460e03d89b929ef39718e7c8419593cdb0
John Bryan Pit M. Acaso <johnbryanpitcasoy@gmail.com>
2022-02-17 17:27:31 +0800

    implemented counter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,61 @@
 import "package:flutter/material.dart";
 
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _count = 0;
+
+  void _increment() {
+    setState(
+      () {
+        _count++;
+      },
+    );
+  }
+
+  void _decrement() {
+    setState(
+      () {
+        _count--;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('$_count',
+            style: const TextStyle(
+              fontSize: 50,
+            )),
+        ElevatedButton(
+          onPressed: _increment,
+          child: const Text("Increment"),
+        ),
+        ElevatedButton(
+          onPressed: _decrement,
+          child: const Text("Decrement"),
+        )
+      ],
+    );
+  }
+}
+
 void main() {
-  runApp(const Center(
-    child: Text(
-      "Hello Flutter!",
-      textDirection: TextDirection.ltr,
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
+      ),
     ),
-  ));
+  );
 }
